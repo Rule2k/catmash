3063c173c9cb4eb87014725929c0d8d813bad66e
Rule2k <nicolaspaviot@gmail.com>
2019-05-30 22:20:11 +0200

    adding to the controller the method to be able to extract from the database only two entries, choosen randomly from the original array

---
+++ back/controller.js
@@ -0,0 +1,33 @@
+// Import
+
+const cats = require('./model');
+
+// Actions permettant de récupérers les données de la BDD. L'action get va être exportée et utilisé dans le fichier routes.js par la suite. 
+// La fonction shuffleArray permet de randomiser l'ordre du tableau de data de la base de données.
+// Par la suite, je slice le tableau avec 2 entrées de moins que la taille totale du tableau (au cas où le nombre de data évolue)
+// Après avoir configuré la route, l'API me renverra donc uniquement deux entrées (au lieu du tableau entier), choisies au hasard, que le front-end utilisera plus tard pour afficher les deux images de chats.
+
+exports.index = (req, res) => {
+  cats.get((err, data) => {
+    function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    }
+    const suffledArray = shuffleArray(data);
+    const twoRandomEntries = suffledArray.slice(data.length - 2);
+    if (err) {
+      res.json({
+        status: 'Erreur',
+        message: err,
+      });
+    }
+    res.json({
+      status: 'Succès',
+      message: 'Deux clés du tableau choisies au hasard',
+      data: twoRandomEntries,
+    });
+  });
+};
