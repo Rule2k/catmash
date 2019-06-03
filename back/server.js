9f40045ce9e8c06091c386572285c320b5f5d91e
Rule2k <nicolaspaviot@gmail.com>
2019-06-03 12:23:59 +0200

    adding new cors origin

--- back/server.js
+++ back/server.js
@@ -11,8 +11,6 @@ const apiRoutes = require('./routes');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:8080' }));
-app.use(cors({ origin: 'http://http://95.142.173.247:8060' }));
 app.use(cors({ origin: 'http://http://95.142.173.247:8080' }));
 
 // BodyParser pour gérer les requêtes POST
