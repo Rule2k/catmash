d414e2f3ef57f3777353e779534bc111bd48bc39
Rule2k <nicolaspaviot@gmail.com>
2019-06-03 12:17:55 +0200

    adding routes to external JS / CSS file (not sure if this is the right way to fix the 404 issue with external file)

--- back/server.js
+++ back/server.js
@@ -28,7 +28,9 @@ const port = process.env.PORT || 8060;
 
 // Message de test de la homepage
 app.get('/', (req, res) => res.sendFile(path.resolve('dist/index.html')));
-
+app.get('/app.css', (req, res) => res.sendFile(path.resolve('dist/app.css')));
+app.get('/vendors.js', (req, res) => res.sendFile(path.resolve('dist/vendors.js')));
+app.get('/app.js', (req, res) => res.sendFile(path.resolve('dist/app.js')));
 
 // Utilisation des routes de l'API
 app.use('/api', apiRoutes);
