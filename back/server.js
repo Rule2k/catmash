cf1afb0e1440cdca854ec18ce5a5e6f843940806
Rule2k <nicolaspaviot@gmail.com>
2019-06-05 15:26:14 +0200

    adding a link to home from clicking on the logo, replacing remotes URL values before deploying

--- back/server.js
+++ back/server.js
@@ -11,7 +11,7 @@ const apiRoutes = require('./routes');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:8080' }));
+app.use(cors({ origin: 'http://95.142.173.247:8080' }));
 
 // BodyParser pour gérer les requêtes POST
 
