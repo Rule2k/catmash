d8167aa6e7ec50dbeb48c2959f88d3d6ee5a1622
Rule2k <nicolaspaviot@gmail.com>
2019-06-05 16:20:06 +0200

    try

--- back/server.js
+++ back/server.js
@@ -30,7 +30,7 @@ const port = process.env.PORT || 8060;
 
 // Liaison des fichiers & routes
 
-app.get('*', (req, res) => res.sendFile(path.resolve('dist/index.html')));
+app.get('*', (req, res) => res.sendFile(path.resolve('index.html')));
 app.use(express.static(path.resolve('dist')));
 // app.get('/', (req, res) => res.sendFile(path.resolve('dist/index.html')));
 
